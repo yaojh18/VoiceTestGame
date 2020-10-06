914828175de06d8ff536a7bb7cde05c70a864aa6
sunyutao32 <sunyt32@163.com>
2020-10-06 15:36:13 +0800

    修改了Dockerfile中的前端运行命令，增加了登录需求以及相关代码

--- WeApp/src/pages/rank/rank.tsx
+++ WeApp/src/pages/rank/rank.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import Taro from '@tarojs/taro'
-import { View, Button } from '@tarojs/components'
+import { View } from '@tarojs/components'
 import './rank.less'
 
 export default class Rank extends Component {
