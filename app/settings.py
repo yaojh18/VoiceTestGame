493da70f52a54bd9ba9f72e2b760a9444e5c27d2
limf <2018011110@secoder.net>
2020-10-08 21:27:28 +0800

    删除旧的应用文件夹mediaAPII

--- app/settings.py
+++ app/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediaAPI/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/media")
 # MEDIA_URL = 'media/'
 
 # Quick-start development settings - unsuitable for production
