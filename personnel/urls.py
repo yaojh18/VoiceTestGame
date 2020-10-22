554e51c473b6557e6e8f81011f976d4c5626e8b9
yaojh18 <yaojh18@mails.tsinghua.edu.cn>
2020-10-22 09:37:43 +0800

    新增微信获取用户列表以及用户音频的接口。

--- personnel/urls.py
+++ personnel/urls.py
@@ -2,11 +2,12 @@
 Register urls here.
 """
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, WechatViewSet
+from .views import UserViewSet, WechatViewSet, LevelViewSet
 
 router = DefaultRouter()
 router.register('users',UserViewSet, basename='users')
 router.register('wechat',WechatViewSet, basename='wechat')
+router.register('level', LevelViewSet, basename='level')
 
 urlpatterns = [
 
