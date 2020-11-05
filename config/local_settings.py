9b57ce295830152d71d712ee4dc101456797648c
yaojh18 <yaojh18@mails.tsinghua.edu.cn>
2020-11-05 12:10:49 +0800

    修改配置，并且添加前段展示的页面。

--- config/local_settings.py
+++ config/local_settings.py
@@ -1,13 +1,11 @@
-import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'db.sqlite3'),
     }
 }
 
-WEAPP_ID = 'wxcec8955125bd6732'
-WEAPP_SECRETE = 'd26321578e183029be05d63ac982a660'
+WEAPP_ID = ''
+WEAPP_SECRETE = ''
 
 APP_URL = 'http://zentao.c-smb.com:23101'
 APP_ID = '13001000002000000000010'
