6ecc076510ad01cfcc299db1a4793bdfe1f14b6d
limf <2018011110@secoder.net>
2020-11-18 07:52:57 +0800

    添加数据分析可视化单元测试 #15

--- media/tests.py
+++ media/tests.py
@@ -3,6 +3,7 @@ Tests of media app
 """
 # pylint: disable=R0913, E5142, C0301
 import os
+from datetime import datetime
 from django.test import TestCase
 from django.contrib.auth.models import Group, User
 from personnel.models import UserProfile, UserAudio
@@ -207,7 +208,7 @@ class ClientMediaTest(TestCase):
 
 class DataAnalysisTest(TestCase):
     """
-    tests for data analysis APIs
+    tests for media data analysis APIs
     """
     def setUp(self):
         Group.objects.create(name='manager')
@@ -310,3 +311,64 @@ class DataAnalysisTest(TestCase):
         self.assertEqual(response.status_code, 200)
         response = self.user_audio(sort='time', end_time='2020-11-5')
         self.assertEqual(response.status_code, 200)
+
+
+class ChartTest(TestCase):
+    """
+    Tests for chart APIs
+    """
+    def setUp(self):
+        Group.objects.create(name='manager')
+        Group.objects.create(name='visitor')
+        self.client.post('/api/users/', data={
+            'username': 'test',
+            'password': '123456',
+            'password_confirm': '123456'
+        }, content_type='application/json')
+        self.client.login(username='test', password='123456')
+        media1 = OriginMedia(title='test1', content='test 1', level_id=0, type_id=1,
+                                   audio_path='/data/origin/audio/test1.wav',
+                                   video_path='/data/origin/video/test1.mp4')
+        media1.save()
+        media2 = OriginMedia(title='test2', content='test 2', level_id=1, type_id=2,
+                                   audio_path='/data/origin/audio/test2.wav',
+                                   video_path='/data/origin/video/test2.mp4')
+        media2.save()
+        user = User(username='user1', password='123456')
+        user.save()
+        UserProfile.objects.create(user=user, openid='hhh', gender=1)
+        UserAudio.create(user=user, media=media1, audio='/data/origin/audio/test6.wav',
+                         timestamp=datetime(2020, 11, 17, 6, 12, 6, 411666), score=86)
+        UserAudio.create(user=user, media=media2, audio='/data/origin/audio/test3.wav',
+                         timestamp=datetime(2020, 11, 18, 6, 12, 6, 411666), score=40)
+        UserAudio.create(user=user, media=media1, audio='/data/origin/audio/test4.wav',
+                         timestamp=datetime(2020, 11, 15, 6, 12, 6, 411666), score=97)
+        UserAudio.create(user=user, media=media2, audio='/data/origin/audio/test5.wav',
+                         timestamp=datetime(2020, 11, 17, 6, 12, 6, 411666), score=79)
+
+    def media_chart(self, data_id):
+        """
+        get data for media charts
+        """
+        return self.client.get('/api/manager/data/origin/'+str(data_id)+'/chart/',
+                               content_type='application/json')
+
+    def user_chart(self):
+        """
+        get data for user charts
+        """
+        return self.client.get('/api/manager/data/user/chart',
+                               content_type='application/json')
+
+    def user_audio_chart(self):
+        """
+        get data for user audio charts
+        """
+        return self.client.get('/api/manager/data/user_audio/chart',
+                               content_type='application/json')
+
+    # def test_media_chart(self):
+    #     """
+    #     tests for media charts
+    #     """
+    #     response =
